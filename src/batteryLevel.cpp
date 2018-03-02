be7581f7f0aca18e9b114c55d27ae610584aad12
Unknown <kcy0511@gmail.com>
2018-03-02 03:40:22 -0500

    Add external variable references
    This allows each class to properly use the class that initiated in main.

--- src/batteryLevel.cpp
+++ src/batteryLevel.cpp
@@ -1,7 +1,5 @@
 #include "batteryLevel.h"
 
-Zumo32U4LCD lcd2;
-Zumo32U4ButtonB buttonB;
 
 Battery::Battery() {}
 
@@ -14,18 +12,18 @@ void Battery::printVoltage() {
     Serial.print(readBatteryMillivolts());
     Serial.print(F(" mV"));
     delay(100);
-    lcd2.clear();
-    lcd2.print(readBatteryMillivolts());
-    lcd2.print(" B->");
-    lcd2.gotoXY(0,1);
+    lcd.clear();
+    lcd.print(readBatteryMillivolts());
+    lcd.print(" B->");
+    lcd.gotoXY(0,1);
 
       if(readBatteryMillivolts()<4000) {
         Serial.println("   Battery Level: Charge Immediately! ");
-        lcd2.print("Charge!");
+        lcd.print("Charge!");
       }
       else {
         Serial.println("   Battery level: Good");
-        lcd2.print("You good");
+        lcd.print("You good");
       }
 
     }
--- src/ysuZumo.h
+++ src/ysuZumo.h
@@ -3,8 +3,14 @@
 
 #include <Wire.h>
 #include <Zumo32U4.h>
+
 #include "batteryLevel.h"
 
 Battery myBattery;
 
+extern Zumo32U4ButtonA buttonA;
+extern Zumo32U4ButtonB buttonB;
+extern Zumo32U4ButtonC buttonC;
+extern Zumo32U4LCD lcd;
+extern Zumo32U4Buzzer buzzer;
 #endif /* end of include guard: YSUZUMO_H */
