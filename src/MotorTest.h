445dc63023cf80a6f98f919ee5ae0a5e6a3ced4a
Unknown <kcy0511@gmail.com>
2018-03-27 16:40:32 -0400

    Add Motor Prototypes

--- src/MotorTest.h
+++ src/MotorTest.h
@@ -6,6 +6,11 @@ class Motors {
 public:
   Motors();
   void MotorTest();
+  void turnRobot(int angle);
+  void driveStraightTime(int time);
+  void driveStraightDistance(int distance);
+  void driveStraight();
+  void stopRobot();
 };
 
 extern Zumo32U4Encoders encoders;
