426ad88b2a3137f2107d691f572705149d36e318
Unknown <kcy0511@gmail.com>
2018-04-07 11:14:51 -0400

    More TurnSensor Uncomments

--- src/MotorTest.h
+++ src/MotorTest.h
@@ -7,7 +7,7 @@ class Motors {
 public:
   Motors();
   void MotorTest();
-//  void turnRobot(int angle);
+  void turnRobot(int angle);
 //  void driveStraightTime(int time);
 //  void driveStraightDistance(int distance);
 //  void driveStraight();
